48129b3310287a39da61524baa4e71205fb904f5
Jules Houben <jules.houben@live.nl>
2023-04-20 10:48:29 +0200

    Add bootstrap and sass to project

---
+++ sevyh/src/main.ts
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+
+
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.scss'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
