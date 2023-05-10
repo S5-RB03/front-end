010c5e79234faf42f6fcdc3da34aabf5c54dabe1
Jules <jules.houben@live.nl>
2023-05-10 14:58:24 +0200

    Update header with login/logout and register buttons

--- sevyh/src/router/index.ts
+++ sevyh/src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import AuthView from '../views/auth/AuthView.vue'
 import ChatView from '../views/ChatView.vue'
 
 const router = createRouter({
@@ -10,11 +9,6 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
-    },
-    {
-      path: '/auth/login',
-      name: 'auth',
-      component: AuthView
     },
     {
       path: '/chat',
