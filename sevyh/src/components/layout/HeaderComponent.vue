0802ba14e598eb2582446205a3f2a14867d22b5e
Jules <jules.houben@live.nl>
2023-05-10 15:42:49 +0200

    Update header

--- sevyh/src/components/layout/HeaderComponent.vue
+++ sevyh/src/components/layout/HeaderComponent.vue
@@ -29,7 +29,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <router-link class="nav-link active btn btn-primary" aria-current="page" to="/">Home</router-link>
+          <router-link class="nav-link btn btn-primary" aria-current="page" to="/">Home</router-link>
         </li>
         <li class="nav-item">
           <router-link to="/chat" class="nav-link btn btn-primary">Chat</router-link>
