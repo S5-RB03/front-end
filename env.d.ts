38e5ee71704979eec45107748bf598ab1c3aab47
Jules <jules.houben@live.nl>
2023-06-09 22:14:43 +0200

    Fix build issue

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+declare global {
+  interface Window {
+    env: {
+      VITE_KEYCLOAK_URL: string;
+      VITE_USERSERVICE_URL: string;
+      VITE_CHATSERVICE_URL: string;
+    };
+  }
+
+  var env: {
+    VITE_KEYCLOAK_URL: string;
+    VITE_USERSERVICE_URL: string;
+    VITE_CHATSERVICE_URL: string;
+  };
+}
