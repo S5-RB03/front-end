7e9a8e956a5220751db818798932b0a72ed2ce3b
Jules <jules.houben@live.nl>
2023-06-09 13:33:00 +0200

    Add loading and retries to userlist

--- src/components/users/UserListComponent.vue
+++ src/components/users/UserListComponent.vue
@@ -1,5 +1,12 @@
 <template>
-    <ul>
+    <div v-if="isLoading" class="spinner-border text-primary" role="status">
+        <span class="visually-hidden">Loading...</span>
+    </div>
+    <div v-else-if="hasError" class="alert alert-danger" role="alert">
+        <p v-if="retryCount < 5">Failed to load users. Retrying in {{ retryTimer }} seconds...</p>
+        <p v-if="retryCount >= 5">No more retries. Please check your connection.</p>
+    </div>
+    <ul v-else>
         <li v-for="user in users" :key="user.uuid">
             <button @click="setReceiverId(user.uuid)">{{ user.uuid }}</button>
         </li>
@@ -25,22 +32,48 @@ type User = {
 };
 
 const users = ref<User[]>([]);
+const isLoading = ref(false);
+const hasError = ref(false);
+const retryCount = ref(0);
+const retryTimer = ref(5);
 
 const fetchData = async () => {
-    try{
-        const response = await fetch('http://localhost:3003/api/v1/users/uuids');
+    isLoading.value = true;
+    try {
+        const response = await fetch(import.meta.env.VITE_USERSERVICE_URL as String + '/v1/users/uuids');
         if(response.ok){
             const jsonResponse = await response.json();
             users.value = jsonResponse;
-            console.log(users.value);
+            hasError.value = false;
+            retryCount.value = 0;
+            isLoading.value = false;
         }else{
-            console.error('Failed to fetch users:', await response.text());
+            throw new Error('Failed to fetch users');
         }
-    }catch(error){
-        console.error('Error fetching users:', error);
+    } catch(error) {
+        hasError.value = true;
+        if (retryCount.value < 5) {
+            retryCount.value += 1;
+            startRetryTimer();
+        } else {
+            hasError.value = true;
+        }
+    } finally {
+        isLoading.value = false;
     }
 };
 
-onMounted(fetchData);
+const startRetryTimer = () => {
+    retryTimer.value = 5;
+    const intervalId = setInterval(() => {
+        if (retryTimer.value > 0) {
+            retryTimer.value -= 1;
+        } else {
+            clearInterval(intervalId);
+            fetchData();
+        }
+    }, 1000);
+};
 
+onMounted(fetchData);
 </script>
